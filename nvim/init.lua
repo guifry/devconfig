c826a8ae052a54711f60a7384192b0941059b26c
Guifry <22219650+guifry@users.noreply.github.com>
2026-06-10 16:00:47 +0100

    feat: add <leader>so to browse org notes

--- nvim/init.lua
+++ nvim/init.lua
@@ -508,6 +508,9 @@ require("lazy").setup({
 			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 			vim.keymap.set("n", "<leader>sc", builtin.commands, { desc = "[S]earch [C]ommands" })
+			vim.keymap.set("n", "<leader>so", function()
+				builtin.find_files({ cwd = "~/org" })
+			end, { desc = "[S]earch [O]rg notes" })
 			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
 
 			-- This runs on LSP attach per buffer (see main LSP attach function in 'neovim/nvim-lspconfig' config for more info,
